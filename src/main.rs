dfa074b6254b10bb072f255dce4dd7afcf08e2ee
Saravut Nakglom <snakglom@gmail.com>
2023-03-16 00:14:42 +0700

    feat: use lambda-web

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use dotenv::dotenv;
+use helper::get_address::get_address;
+use lambda_web::{is_running_on_lambda, run_hyper_on_lambda, LambdaError};
 
 mod handler;
 mod helper;
 mod router;
 
 #[tokio::main]
-async fn main() -> Result<(), lambda_http::Error> {
+async fn main() -> Result<(), LambdaError> {
     dotenv().ok();
 
     tracing_subscriber::fmt::init();
 
-    lambda_http::run(router::app_router()).await
+    match is_running_on_lambda() {
+        true => run_hyper_on_lambda(router::app_router()).await?,
+        false => {
+            if let Err(error) = axum::Server::bind(&get_address())
+                .serve(router::app_router().into_make_service())
+                .await
+            {
+                eprintln!("{}", error)
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/router.rs
+++ src/router.rs
@@ -1,11 +1,10 @@
 use crate::handler::health::health_handler;
 use axum::{routing, Router};
-use lambda_http::Body;
 
-pub fn public_router() -> Router<(), Body> {
+pub fn public_router() -> Router {
     Router::new().route("/health", routing::get(health_handler))
 }
 
-pub fn app_router() -> Router<(), Body> {
+pub fn app_router() -> Router {
     Router::new().nest("/api", public_router())
 }
